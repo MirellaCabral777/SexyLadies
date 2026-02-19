89332c17da06c3bca879f96b4595fab4edfd4114
MirellaCabral777 <matheusfiuzaa.mc@gmail.com>
2026-02-19 18:02:02 -0300

    Etapa 3 pagina cadastro visual

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const bcrypt = require("bcryptjs");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
