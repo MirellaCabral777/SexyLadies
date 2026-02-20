9ec992e9826f62d7555bf4a02bb2afa56f603fd4
MirellaCabral777 <matheusfiuzaa.mc@gmail.com>
2026-02-20 10:05:17 -0300

    teste raiz

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
 
-app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.send("FUNCIONOU 🔥");
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Servidor rodando");
