11f61d963729b964dd7289bef987a36b048e2cd0
MirellaCabral777 <matheusfiuzaa.mc@gmail.com>
2026-02-20 10:16:28 -0300

    porta correta render

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("FUNCIONOU 🔥");
+// servir arquivos da pasta public
+app.use(express.static(path.join(__dirname, "public")));
+
+// teste simples
+app.get("/api/teste", (req, res) => {
+  res.json({ ok: true });
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Servidor rodando");
+// 🔥 ESSA LINHA É OBRIGATÓRIA NO RENDER
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
 });
