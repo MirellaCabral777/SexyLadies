f96a3484609bd2643b7ced84d25951409f7d76a4
MirellaCabral777 <matheusfiuzaa.mc@gmail.com>
2026-02-20 09:38:30 -0300

    teste static

--- server.js
+++ server.js
@@ -3,15 +3,8 @@ const path = require("path");
 
 const app = express();
 
-// 🔥 ESSA LINHA É O QUE FAZ FUNCIONAR
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/api/teste", (req, res) => {
-  res.json({ ok: true });
-});
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log("Servidor rodando na porta " + PORT);
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor rodando");
 });
